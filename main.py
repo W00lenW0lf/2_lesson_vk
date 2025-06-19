b4cfd190dfa77855c013887c38728b1220b47f46
W00lenW0lf <igor45641@gmail.com>
2025-06-19 18:45:39 +0700

    work on mistakes

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import requests
 import os
+from urllib.parse import urlparse
+from dotenv import load_dotenv
 
-TOKEN = os.environ["TOKEN"]
 
-
-def link_stats(token, key):
+def get_link_click_stats(token, key):
     payload = {"access_token": token,
                "key": key,
                "interval": "month",
@@ -12,55 +12,47 @@ def link_stats(token, key):
                'v': '5.131'}
     response = requests.get(f"https://api.vk.com/method/utils.getLinkStats", params=payload)
     response.raise_for_status()
-    data = response.json()
-    if "error" in data:
-        link_stats_output = "Ошибка:" +  data["error"]["error_msg"]
+    link_statistic_data = response.json()
+    if "error" in link_statistic_data:
+        return None
     else:
-        stats = data["response"]["stats"]
+        stats = link_statistic_data["response"]["stats"]
         if stats:
             views = stats[0]["views"]
-            link_stats_output = "Количество кликов за неделю:" + str(views)
+            return "Количество кликов за месяц:" + str(views)
         else:
-            link_stats_output = "Нет данных о переходах."
-    return link_stats_output
+            return "Нет данных о переходах."
 
-
-def shorten_link(token, url):
+def get_shorten_link(token, url):
     payload = {"access_token": token,
                "url": url,
                'v': '5.131'}
     response = requests.get(f"https://api.vk.com/method/utils.getShortLink", params=payload)
     response.raise_for_status()
-    data = response.json()
-    if "error" in data:
-        shorten_link_output = "Ошибка:" + data["error"]["error_msg"]
+    link_shortener_data = response.json()
+    if "error" in link_shortener_data:
+        return None
     else:
-        shorten_link_output = data["response"]["short_url"]
-    return shorten_link_output
+        return link_shortener_data["response"]["short_url"]
+
+def is_link_shorten(url):
+    return urlparse(url).netloc == "vk.cc"
 
 
-def is_shorten_link(token, url):
-    if url[0:14] == "https://vk.cc/":
-        link_status = "short"
-    else:
-        link_status = "long"
-    return link_status
-
-
-def main(token):
+def main():
+    load_dotenv()
+    token = os.environ["VK_SERVICE_TOKEN"]
     url = input('Введите ссылку, если она короткая то программа выдаст количество переходов по ней,'
                 ' если нет - сократит:  ')
-    link_status = is_shorten_link(token, url)
-    if link_status == "short":
+    link_length = is_link_shorten(url)
+    if link_length:
         key = url.replace("https://vk.cc/", "")
-        link_statistic = link_stats(token, key)
+        link_statistic = get_link_click_stats(token, key)
         print(link_statistic)
-    elif link_status == "long":
-        short_link = shorten_link(token, url)
-        print(short_link)
     else:
-        print("Ссылка недействительна")
+        short_link = get_shorten_link(token, url)
+        print(short_link)
 
 
 if __name__ == '__main__':
-    main(TOKEN)
+    main()
